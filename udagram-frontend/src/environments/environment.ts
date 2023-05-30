72ebc5b9bc3d247e7c4f4a9eed1ec0920ec68da3
Thien Nguyen <nqthien041292@gmail.com>
2023-05-31 01:52:48 +0700

    Update config for Host API

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0d31c4dd1ac34240b9445581232199f-1737201988.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
